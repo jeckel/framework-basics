b8840420a348b227f0b2f5cb34a9bc0e4d5f66d6
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2021-06-02 14:37:33 +0200

    Add QA tools

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 02/06/2021
+ */
---
+++ src/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
+ * Created at: 02/06/2021
+ */
+
+declare(strict_types=1);
+
+namespace App;
+
+class Kernel
+{
+
+}
