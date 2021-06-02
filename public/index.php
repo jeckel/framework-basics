750e4cb5947e6bcdc3c53d7cabdab4e08f9d9f59
Julien Mercier-Rojas <julien@jeckel-lab.fr>
2021-06-02 15:02:24 +0200

    Hello world

--- public/index.php
+++ public/index.php
@@ -4,3 +4,13 @@
  * @author: Julien Mercier-Rojas <julien@jeckel-lab.fr>
  * Created at: 02/06/2021
  */
+
+?>
+<html lang="fr">
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+</body>
+</html>
